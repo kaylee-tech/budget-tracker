47fd85fa674644ed8be6f86de2ce8478b65c47aa
Megan Hayes <kaylee.hayes007@gmail.com>
2023-10-14 16:51:18 +0200

    Finished section 7,8 and 9

--- budget_tracking.py
+++ budget_tracking.py
@@ -166,6 +166,8 @@ def display_expenses():
         print("There are no expenses in the table right now\n")
     else:
         print(str_expenses)
+    db.commit()
+    db.close() 
         
 
 # Displays expenses if they fall into the selected category
@@ -247,7 +249,7 @@ def pulling_income():
     return id_list, category_num_list, amount_list
 
 
-# Displays all the titles of the book
+# Displays the incomes from the income table
 def display_income():
     """
     Displays all the income records from the income table
@@ -268,6 +270,8 @@ def display_income():
         print("There is no income in this table\n")
     else:
         print(str_income)
+    db.commit()
+    db.close() 
 
 
 # Displays incomes if they fall into the selected category
@@ -304,7 +308,7 @@ def add_income(id, category_number, amount):
 
     :param int id: The unique identify ID 
     :param int category_number: The category number
-    :param int amount: The cost of the new incoe
+    :param int amount: The cost of the new income
     """
     db = sqlite3.connect('budgets')
     cursor = db.cursor()
@@ -312,6 +316,193 @@ def add_income(id, category_number, amount):
                    VALUES(?,?,?)''', (id, category_number, amount))
     db.commit()
     print("income has been added to the database\n")
+    db.close()
+
+
+# Organises data from database into seperate lists
+def pulling_budgets():
+    """
+    Pulling all the data from the budget table and putting the data in 
+    seperate appropriate lists
+
+    :returns: All the budgets' ids
+    :rtype: list
+
+    :returns: All the budgets' category numbers
+    :rtype: list
+
+    :returns: All the expenses budgets' limits
+    :rtype: list
+    """
+    # empty lists to put data in them
+    id_list = []
+    category_num_list = []
+    bud_limit = []
+
+    # Reading in information from Database
+    db = sqlite3.connect('budgets')
+    cursor = db.cursor()
+    cursor.execute('''SELECT id, category_number, expenses_budget
+                    FROM budget ''')
+    # Putting data in appropriate lists
+    for row in cursor:
+        id_list.append(row[0])
+        category_num_list.append(row[1])
+        bud_limit.append(row[2])
+    db.commit()
+    db.close()   
+    return id_list, category_num_list, bud_limit
+
+
+# Displays th budgets from the budget table
+def display_budgets():
+    """
+    Displays all the budget records from the budget table
+    """
+    print("list of budgets")
+    st1 = "Category_number"
+    st2 = "Expense Budget"
+    str_bud =f"ID\t{st1}\t\t{st2}\n"
+
+    # Reading in information from Database
+    db = sqlite3.connect('budgets')
+    cursor = db.cursor()
+    cursor.execute('''SELECT id, category_number, expenses_budget
+                    FROM budget ''')
+    # Displaying data out line by lie
+    num_counter = 0
+    for row in cursor:
+        str_bud += '{0}\t{1}\t\t\t{2}\n'.format(row[0],row[1],row[2])
+        num_counter += 1
+    if num_counter == 0:
+        end_str = "There are no budget records in this table\n"
+        return end_str
+    else:
+        return str_bud
+    db.commit()
+    db.close() 
+
+
+# Displays budget records if they fall into the selected category
+def display_some_budgets(category_num):
+    """
+    Displays some of the budget records from the budget table if they fall 
+    under the user selected category
+
+    :param int category_num: The user selected category
+    """
+    print("list of budgets")
+    st1 = "Category_number"
+    st2 = "Expense Budget"
+    str_bud =f"ID\t{st1}\t\t{st2}\n"
+    # Reading in data from database
+    db = sqlite3.connect('budgets')
+    cursor = db.cursor()
+    cursor.execute('''SELECT id, category_number, expenses_budget FROM budget 
+                   WHERE category_number = ? ''', (category_num,))
+    num_counter = 0
+    for row in cursor:
+        str_bud += '{0}\t{1}\t\t\t{2}\n'.format(row[0],row[1],row[2])
+        num_counter += 1
+    db.commit()
+    db.close()
+    if num_counter == 0:
+        print("There are no budget records under this category\n")
+    else:
+        print(str_bud)
+
+
+# Displays the empty budgets
+def display_empty_budgets():
+    """
+    Displays the categories that have no budget from the budget table 
+    """
+    id_list, budget_category, budget_cost = pulling_budgets()
+    options_list = category_number_list()
+    category_list = pull_categories()
+    space_for_new_bud = True
+    if len(id_list) == len(options_list):
+        space_for_new_bud = False
+
+    # Printing out list of empty budget categories
+    if(space_for_new_bud == True):
+        print("list of empty budgets")
+        st1 = "Category_number"
+        str_bud =f"ID\t{st1}\n"
+
+        num_checker = 0
+        num_count = 0
+        while len(options_list) > num_count:
+            if options_list[num_count] in budget_category:
+                pass
+            else: 
+                num_bud = str(num_count+1)
+                str_bud += (f"{num_bud}\t{category_list[num_count]}\n")
+                num_checker += 1   
+            num_count += 1
+    # Results
+    if num_checker > 0:
+        return str_bud
+    else:
+        str_empty = "list of empty budgets\nThere are no empty budgets"
+        return str_empty
+               
+
+# Creating a list of numbers that correspond with how many categories there are
+def empty_budget_number_list():
+    """
+    Pulls the data out of the numbers corresponding with the category table and
+    records corresponding number of category not in budget table
+
+    :returns: numbers that represent categories not in the budget table
+    :rtype: list
+    """
+    id_list, budget_category, budget_cost = pulling_budgets()
+    options_list = category_number_list()
+    num_list = []
+
+    num_count = 0
+    while len(options_list) > num_count:
+        if options_list[num_count] in budget_category:
+            pass
+        else:
+            num_list.append(num_count + 1)
+        num_count += 1
+    return num_list
+   
+
+# Adding budget to the budget table
+def add_budget(id, category_number, budget):
+    """
+    Adds the new budget to the budget table in the database
+
+    :param int id: The unique identify ID 
+    :param int category_number: The category number
+    :param int budget: The amount set of the new budget
+    """
+    db = sqlite3.connect('budgets')
+    cursor = db.cursor()
+    cursor.execute('''INSERT INTO budget(id, category_number, expenses_budget) 
+                   VALUES(?,?,?)''', (id, category_number, budget))
+    db.commit()
+    print("budget has been added to the database\n")
+    db.close()
+
+
+# Updating a budget record in the budget table
+def update_budget(id, budget):
+    """
+    Updates a budget record of the budget table in the database
+
+    :param int id: The unique identify ID of budget getting updated
+    :param int budget: The new amount of the updated budget
+    """
+    db = sqlite3.connect('budgets')
+    cursor = db.cursor()
+    cursor.execute('''UPDATE budget SET expenses_budget=? WHERE id=?''', 
+                   (budget, id))
+    db.commit()
+    print("budget has been updated in the database\n")
     db.close()
 
 
@@ -327,11 +518,12 @@ while True:
                          4. Add income
                          5. View all incomes
                          6. View income by category
-                         7. Set budget for a category 
-                         8. View budget for a category
-                         9. Set financial goals
-                         10. View progress towards financial goals
-                         11. Quit
+                         7. Set or update a budget for a category 
+                         8. View all budgets
+                         9. View budget for a category
+                         10. Set financial goals
+                         11. View progress towards financial goals
+                         12. Quit
                          : ''')
     print("")
 
@@ -495,7 +687,7 @@ while True:
             amount = value_err(f"Please enter how you earn from {cate_obj}: ")
             add_income(id, id_category, amount)
 
-    # Allows user to view their income from a selected category
+    # Allows user to view their incomes
     elif menu == 5:
         display_income()
 
@@ -505,7 +697,7 @@ while True:
         print("\nWhich category do you want to view your income from?")
         s1 = """Select the number of the category\n"""
         menu_cate = value_err(f'''{s1}{display_categories()}: ''')
-         # Making sure user selected an existing option
+        # Making sure user selected an existing option
         user_bool = False
         options_list = category_number_list()
         while(user_bool == False):
@@ -520,22 +712,123 @@ while True:
 
     # Set their budget for a selected category
     elif menu == 7:
-        pass
+        print(display_budgets())
+
+        id_list, budget_category, budget_cost = pulling_budgets()
+        options_list = category_number_list()
+        space_for_new_bud = True
+        if len(id_list) == len(options_list):
+            space_for_new_bud = False
+
+        # Seeing if the user wants to add a new category or use existing one
+        menu_7_bool = False
+        while menu_7_bool == False:
+            if(space_for_new_bud == True):
+                menu_for_7 = value_err('''Select one of the following options
+                                1. Set budget for a category
+                                2. Update a budget for a category
+                                : ''')
+            else:
+                menu_for_7 = value_err('''Select one of the following options
+                            1. Option not available because all budgets exist
+                            2. Update a budget for a category
+                            : ''')
+            if menu_for_7 == 1 and space_for_new_bud == True:
+                menu_7_bool = True
+            
+            elif menu_for_7 == 2:
+                menu_7_bool = True
+            
+            else:
+                print("\nPlease only enter 1 or 2") 
+            
+        # Adding budget for a category
+        if menu_for_7 == 1:
+            print("")
+            print("Which Category do you want to add a budget for?")
+            s1 = """Select the number of the category\n"""
+            menu_cate = value_err(f'''{s1}{display_empty_budgets()}: ''')
+
+            # Making sure user selected an existing option
+            user_bool = False
+            options_list = empty_budget_number_list()
+            while(user_bool == False):
+                # Checking that the category exists
+                if(menu_cate in options_list):
+                    user_bool = True
+                else:
+                    print("\nPlease only select a number from the options")
+                    menu_cate = value_err(f'''{s1}{display_empty_budgets()}: ''')
+
+            # Getting info from user to add to database
+            category_list = pull_categories()
+            print(f"\nAdding an budget for {category_list[menu_cate-1]}")
+            budget = value_err("Please enter what the limit should be: ")
+            id_l, cate_num_l, bud_limit = pulling_budgets()
+            id = len(id_l) + 1
+
+            # Adding budget to budget table
+            add_budget(id, menu_cate, budget)
+
+        # Updating budget for a category
+        if menu_for_7 == 2:
+            id_list, cate_num_bud, budget_amount = pulling_budgets()
+
+            # Checking to see if anything can be updated
+            if len(id_list) == 0:
+                print("Can't update anything - There is nothing to update\n")
+            else:
+                print("\nWhich budget do you want to update?")
+                s1 = """Select the Id of the category\n"""
+                menu_cate = value_err(f'''{s1}{display_budgets()}: ''')
+
+                # Making sure user selected an existing option
+                user_bool = False
+                while(user_bool == False):
+                    if(menu_cate in id_list):
+                        user_bool = True
+                    else:
+                        print("\nPlease only select a number from the options")
+                        menu_cate = value_err(f'''{s1}{display_budgets()}: ''')
+                print("") 
+
+                # Updating a budget record
+                new_budget = value_err("Enter the amount of the new budget: ")
+                update_budget(menu_cate, new_budget)      
+
+    # View all their budgets
+    elif menu == 8:
+        print(display_budgets())
 
     # Allows user to see their budget for a selected category
-    elif menu == 8:
-        pass
+    elif menu == 9:
+        # Selecting the category
+        print("\nWhich category do you want to view your budgets from?")
+        s1 = """Select the number of the category\n"""
+        menu_cate = value_err(f'''{s1}{display_categories()}: ''')
+         # Making sure user selected an existing option
+        user_bool = False
+        options_list = category_number_list()
+        while(user_bool == False):
+            if(menu_cate in options_list):
+                user_bool = True
+            else:
+                print("\nPlease only select a number from the options")
+                menu_cate = value_err(f'''{s1}{display_categories()}: ''')
+
+        print("")       
+        display_some_budgets(menu_cate)
 
     # Allows user to set finacial goals 
-    elif menu == 9:
-        pass
-
-    # Allows user to view their progress towords their finacial goals
     elif menu == 10:
         pass
 
-    # Exits the program
+    # Allows user to view their progress towords their finacial goals
     elif menu == 11:
+        pass
+
+    # Exits the program
+    elif menu == 12:
         print("Goodbye!")
         exit()
 
